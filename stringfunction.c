4c5e3b580a61aba32c3ebcac9b22c637ddaf1ad7
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-21 16:02:21 +0530

    stringfunction.c

---
+++ stringfunction.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20],b[20];
+int i,j,count1,count2;
+for(i=0;i<n;i++)
+{
+scanf("%s",&a[i]);
+count1+=1;
+}
+for(j=0;j<n;j++)
+{
+scanf("%s",&b[j]);
+count2+=1;
+}
+if(count1==count2)
+{
+printf("%s",a[i]);
+}
+else if(count1<count2)
+{
+printf("%s",b[i]);
+}
+else
+{
+printf("%s",a[i]);
+}
